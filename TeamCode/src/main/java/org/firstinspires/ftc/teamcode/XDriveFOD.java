d81f77610e268d6ffefe5443ad6b5ab9cf5ea465
Augusto <augustogasparetto10@gmail.com>
2025-06-05 08:58:11 -0300

    FieldOrientedDrive no chassi XDrive v1.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDriveFOD.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp
+public class XDriveFOD extends OpMode {
+    DcMotor motorFL;
+    DcMotor motorFR;
+    DcMotor motorBL;
+    DcMotor motorBR;
+    DcMotor coreHexL;
+    DcMotor coreHexR;
+    Servo garra;
+    Servo intake;
+    IMU imu;
+
+    @Override
+    public void init() {
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+        ));
+        imu.initialize(parameters);
+
+        motorFL = hardwareMap.get(DcMotor.class, "FL");
+        motorFR = hardwareMap.get(DcMotor.class, "FR");
+        motorBL = hardwareMap.get(DcMotor.class, "BL");
+        motorBR = hardwareMap.get(DcMotor.class, "BR");
+        coreHexL = hardwareMap.get(DcMotor.class, "CHexL");
+        coreHexR = hardwareMap.get(DcMotor.class, "CHexR");
+        garra = hardwareMap.get(Servo.class, "garra");
+        intake = hardwareMap.get(Servo.class, "intake");
+
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        coreHexL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        coreHexR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        coreHexL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        coreHexR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        Deadline gamepadRateLimit = new Deadline(500, TimeUnit.MILLISECONDS);
+
+        telemetry.addData("Hardware: ", "Initialized");
+    }
+
+    // public void init_loop() {}
+    // public void start(){}
+
+    @Override
+    public void loop(){
+        telemetry.addData("Hardware: ", "Running");
+
+        double drive = gamepad1.left_stick_y;  // frente e atrás
+        double turn = -gamepad1.right_stick_x;  // gira
+        double strafe = -gamepad1.left_stick_x; // direita e esquerda
+
+        double max = Math.max(Math.abs(strafe) + Math.abs(drive) + Math.abs(turn), 1);
+
+        double drivePower = 1 - (0.5 * gamepad1.right_trigger);
+        if(gamepad1.left_bumper) imu.resetYaw();
+
+        telemetry.addLine("Angulo do robô: "+ imu.getRobotYawPitchRollAngles().getYaw());
+
+        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double adjustedStrafe = drive * Math.sin(heading) + strafe * Math.cos(heading);
+        double adjustedDrive = drive * Math.cos(heading) - strafe * Math.sin(heading);
+
+        motorFL.setPower(((adjustedDrive + adjustedStrafe + turn) / max) * drivePower);
+        motorFR.setPower(((adjustedDrive - adjustedStrafe - turn) / max) * drivePower);
+        motorBL.setPower(((adjustedDrive - adjustedStrafe + turn) / max) * drivePower);
+        motorBR.setPower(((adjustedDrive + adjustedStrafe - turn) / max) * drivePower);
+        telemetry.update();
+    }
+}
